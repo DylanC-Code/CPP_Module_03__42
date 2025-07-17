38c50740c3f160d27ebbffdb433a163c9c1dd634
DylanC-Code <dylanc.code@gmail.com>
2025-07-17 22:28:48 +0200

    feat(ex01): main tests done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,20 +6,24 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 21:49:19 by dcastor           #+#    #+#             */
-/*   Updated: 2025/07/17 21:51:09 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/07/17 22:27:52 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ClapTrap.hpp"
+#include "ScavTrap.hpp"
+
 int main(void)
 {
-    ClapTrap dd("DD");
+    ScavTrap dd("DD");
 
     dd.attack("TOTO");
+    dd.guardGate();
     dd.takeDamage(3);
     dd.beRepaired(2);
     dd.takeDamage(20);
     dd.beRepaired(100);
+    dd.guardGate();
+    dd.takeDamage(1000);
     dd.attack("TATA");
     return 0;
 }
