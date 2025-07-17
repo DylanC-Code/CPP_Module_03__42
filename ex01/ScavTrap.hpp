aad3baa6fff4c6cf05ed8e1e12dfcfa98164b841
DylanC-Code <dylanc.code@gmail.com>
2025-07-17 22:28:25 +0200

    feat(ex01): ScavTrap declarations done

--- ex01/ScavTrap.hpp
+++ ex01/ScavTrap.hpp
@@ -6,21 +6,24 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 21:59:07 by dcastor           #+#    #+#             */
-/*   Updated: 2025/07/17 22:01:30 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/07/17 22:26:09 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef __SCAVTRAP_H__
 #define __SCAVTRAP_H__
-#include <string>
 
-class ScavTrap
+#include <string>
+#include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
 {
 public:
     ScavTrap(std::string name);
     ~ScavTrap();
 
     void guardGate();
+    void attack(const std::string &target);
 };
 
 #endif
