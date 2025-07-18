72dda7361c28ab99435de0decefab7b936510aad
dcastor <dcastor@student.42.fr>
2025-07-18 10:32:51 +0200

    fix(ex0*): reliking

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,23 +6,23 @@
 /*   By: dcastor <dcastor@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/17 21:49:19 by dcastor           #+#    #+#             */
-/*   Updated: 2025/07/17 22:27:52 by dcastor          ###   ########.fr       */
+/*   Updated: 2025/07/18 10:28:37 by dcastor          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ScavTrap.hpp"
+#include "FragTrap.hpp"
 
 int main(void)
 {
-    ScavTrap dd("DD");
+    FragTrap dd("DD");
 
     dd.attack("TOTO");
-    dd.guardGate();
+    dd.highFivesGuys();
     dd.takeDamage(3);
     dd.beRepaired(2);
     dd.takeDamage(20);
     dd.beRepaired(100);
-    dd.guardGate();
+    dd.highFivesGuys();
     dd.takeDamage(1000);
     dd.attack("TATA");
     return 0;
